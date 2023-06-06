00f8a1f6c2ed110e582151b6e724a421cfc45227
valclassdevelop <developervalclass.com>
2023-06-06 23:47:59 +0700

    midtrans and google auth

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+
+app.use(cors())
+
+app.use(session({ secret: 'YOUR_SESSION_SECRET', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Serialize user object
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Deserialize user object
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+
+// Koneksi ke MongoDB
+const mongoURI = 'mongodb+srv://dragme:HBXrSHZaJqemsDtW@cluster0.oadoa02.mongodb.net/?retryWrites=true&w=majority'; // Ganti dengan URL MongoDB Anda
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => console.error('Error connecting to MongoDB', error));
+
+// Middleware untuk parsing body request
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// Import rute
+const userRoutes = require('./src/routes/userRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const paymentRoutes = require('./src/routes/paymentRoutes');
+
+// Gunakan rute
+app.use('/payment', paymentRoutes);
+app.use('/api/users', userRoutes);
+app.use('/auth', authRoutes);
+
+// Tangani kesalahan jika rute tidak ditemukan
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(3002,() => {
+    console.log(`Server running in port 3002`)
+})
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,34 @@
 const mongoose = require('mongoose');
 
+function generateRandomString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  
+  return result;
+}
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, minlength: 3 },
+  username: { type: String, required: true, minlength: 3, },
+  idAccount: {type: String, default: null},
+  email: { type: String, required: true, unique: true,
+    validate: {
+      validator: function (value) {
+        // Use a regular expression to validate the email format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
+      },
+      message: 'Invalid email format',
+    },
+  },
   password: { type: String, required: true, minlength: 5 },
-  // tambahkan lebih banyak field sesuai kebutuhan
+  status: { type: String, default: 'standar' }
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+082318908291
