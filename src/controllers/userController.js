70829dfe8605e4030d654d08c8c9f2eb4e804566
valclassdevelop <developervalclass.com>
2023-06-12 20:06:39 +0700

    add dd JWT and bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Mengambil daftar semua pengguna
 const getAllUsers = async (req, res) => {
@@ -22,12 +24,24 @@ const loginUser = async (req, res) => {
     }
 
     // Periksa kecocokan password
-    if (user.password !== password) {
-      return res.status(401).json({ message: 'Password salah' });
-    }
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        console.error('Error comparing passwords:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Passsword salah!' });
+      }
+
+      // Create and sign the JWT token
+      const token = jwt.sign({ userId: user._id }, 'Swiftvel', { expiresIn: '1h' });
+
+      // Return the token to the client
+      return res.status(200).json({ message: 'Login berhasil', token });
+    });
 
     // Berhasil login
-    return res.status(200).json({ message: 'Login berhasil' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Terjadi kesalahan server' });
@@ -38,8 +52,13 @@ const loginUser = async (req, res) => {
 const createUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const user = new User({ username, email, password });
+    
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    const user = new User({ username, email, hashedPassword });
     await user.save();
+
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ message: error });
