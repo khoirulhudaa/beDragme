4077de822b39fb2d4538881ad8ad95241789115b
valclassdevelop <developervalclass.com>
2023-06-13 22:02:51 +0700

    integrasi API

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,6 +4,6 @@ const { callback, pay, cancelOrder } = require('../controllers/paymentController
 
 router.post('/callback', callback);
 router.post('/', pay);
-router.get('/cancel/order/:order_id', cancelOrder);
+router.post('/cancel/order/:order_id', cancelOrder);
 
 module.exports = router;
