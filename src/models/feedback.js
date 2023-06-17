186628e5cf720b6e216470a47c7b1807e699254c
valclassdevelop <developervalclass.com>
2023-06-18 00:30:25 +0700

    fix

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now() },
-  feedack: { type: String, required: true }
+  feedback: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Feedback', feedbackSchema);
